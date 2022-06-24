7bfc1ea0323350a3a293deb52d951f5466ee3488
Michael Johnson <mjj.yoyogi@gmail.com>
2022-06-23 22:47:45 -0400

    Created javascript for calculator, adjusted CSS styling

--- script.js
+++ script.js
@@ -1,9 +1,83 @@
 let inputBox = document.getElementById('display');
 
+let newNumber = false;
+
+let GLOBALNUMBER = '';
+
+let GLOBALSYMBOL = '';
+
+let afterEquals = false;
+
+let equation = [];
+
+let lastUsedKey = ''
+
+
+function solveEquation(equation){
+
+    let toSolve = Number(equation[0])
+    let operator = ''
+    for (let i = 1; i < equation.length; i++){
+        if (isNaN(equation[i])){
+            operator = equation[i]
+        }else{
+            let num = Number(equation[i])
+
+            switch (operator){
+                case '+':
+                    toSolve += num;
+                    break;
+                case '-':
+                    toSolve -=num;
+                    break;
+                case '/':
+                    toSolve = toSolve / num;
+                    break;
+                case '*':
+                    toSolve = toSolve * num;
+                    break;
+            }
+        }
+    }
+    return(toSolve)
+}
+
+function makeNegative(){
+
+    if (inputBox.value.includes('-')){
+        inputBox.value = inputBox.value.substring(1);
+    }else{
+        inputBox.value = '-' + inputBox.value
+    }
+
+
+}
+
+
 
 function addNumber (num){
-    inputBox.value += `${num}`;
-    console.log("bruh")
+    if (afterEquals){
+        inputBox.value = ''
+        equation = []
+        afterEquals = false
+    }
+
+    if (newNumber){
+        equation.push(GLOBALNUMBER)
+
+        equation.push(GLOBALSYMBOL)
+
+        inputBox.value = ''
+        inputBox.value += `${num}`
+
+        newNumber = false
+
+
+    }else{
+        inputBox.value += `${num}`;
+
+    }
+    lastUsedKey = 'num'
 }
 
 function deleteChar(){
@@ -11,4 +85,57 @@ function deleteChar(){
     inputBox.value = deleted
 
 }
+
+function eval(symbol){
+
+    if (inputBox.value !== ''){
+        
+        switch (symbol){
+
+            case "=":
+                if (lastUsedKey !== '='){
+                    let num = inputBox.value
+
+                    equation.push(num)
+    
+                    let solution = solveEquation(equation);
+    
+                    inputBox.value = solution;
+    
+                    afterEquals = true;
+                    lastUsedKey = '='
+                }else{   
+                }
+                break;
+    
+            case "Clear":
+                equation = []
+                inputBox.value = ''
+
+                lastUsedKey = 'Clear'
+                break;
+    
+            default:
+                GLOBALSYMBOL = symbol
+                newNumber = true
+                GLOBALNUMBER = inputBox.value
+                lastUsedKey = 'operator'
+
+    
+        }
+
+
+    }else{
+    }
+
+
+
+
+
+
+}
+
+
+
+
 
